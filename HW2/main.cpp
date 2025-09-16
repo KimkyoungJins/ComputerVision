a76e87d41ec5325e49880260e7eb6e237d690dd7
김경건 <kkjin722@gmail.com>
2025-09-16 15:10:41 +0900

    feat: negative image

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -5,13 +5,13 @@
 using namespace cv;
 
 // 이미지에 대한 정보를 변수로 받아서 90도 로테이트 
-// 포인터로 받으면 좀 더 빠를 듯
-void rotate_img(int *rows, int *cols){
-    for (int i = 0; i < *rows; i++){
-        for (int j = 0; j < *cols; j++)
-    
-
-    }
+Mat rotate_90_img(Mat image){
+        for(int i = 0; i < image.rows; i++){
+            for (int j = 0; j < image.cols; j++){
+                image.at<uchar>(j, image.rows - 1 - i) = image.at<uchar>(i, j);
+            }
+        }
+    return image;
 }
 
 int main(){
@@ -19,28 +19,18 @@ int main(){
     // Read an image “lena.png” as a gray-scale image
     // 0은 gray scale으로 읽으라는 것 
     Mat image;
-    int *p;
-    
     image = imread("lena.png", 0);
     imshow("image before rotation", image);
 
-    int *cols, *rows;
-    cols = &image.cols;
-    rows = &image.rows;
-
-    for(int i = 0; i < image.rows; i++){
-        for (int j = 0; j < image.cols; j++){
-            image.at<uchar>()
-        }
-    }
-
     // Generate a 90-degree rotated image
-
-
+    Mat rotated_image = rotate_90_img(image);
+    imshow("Rotated image", rotated_image);
     
     // On the generated image, perform negative transformation 
     // if the pixel value is smaller than 127. 
     // Otherwise, perform gamma transformation with gamma as 10
+    Mat negative_image = rotated_image.clone();
+    
     
 
     // Display the input and the result on two windows
