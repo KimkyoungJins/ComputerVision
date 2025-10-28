1d74b3dba77ba19fc908e02cf83cdb1595af870c
김경건 <kkjin722@gmail.com>
2025-10-28 14:40:37 +0900

    Feat: p2

--- P2/main.cpp
+++ P2/main.cpp
@@ -6,51 +6,68 @@ using namespace std;
 
 // 21900066 김경진
 // 컴퓨터비전 프로젝트 2
-
-
 // 차선 이탈을 감지하기 위한 함수
 // houghline을 사용하면 된다. 
 // 해당 차선 침범을 감지하기 위해서는 
 // 차선을 감지 할 줄 알아야 하고
 // 감지한 차선이 영상의 중앙부분에 들어가기 시작할때 
-// 영상 중앙 부분은 x와 y가 100이라고 차면 x값은 40 ~ 60, y값은 0 10일때 line departure을 작동시킨다. 
-
+// 영상 중앙 부분은 x와 y가 100이라고 차면 x값은 40 ~ 60, y값은 0 20일때 line departure을 작동시키는 것으로 한다. 
+// 가우시안 블러를 통하여서 노이즈를 제거하고
+// 캐니엣지 적용시켜서 라인인 것을 확확인하고
+// 허프변환 통하여서 차선인 것을 인식시킨다. 
+// 그 후에 결과값의 참/거짓을 통하여서 Line departure 출력여부 결정한다
 void line_departure(Mat frame){
-    // Houghlines를 위한 
-    Mat image, edge, result;
-    vector<Vec4i> lines;
 
-    result = frame.clone();
-    Canny(image, edge, 50, 200, 3);
+    Mat line;                   // 허프변환을 위한 
+    int roi_x, roi_y;           // 정해진 roi값 내에서 감지를 해야한다.
+    Mat blur;                   // 가우시안 블러 저장 변수
+    vector<Vec4i> lines;        // 허브변환 벡터값
 
+    // 해당 이미지의 전체 x값과 y값을 모른다. 
+    roi_x = frame.cols;
+    roi_y = frame.rows;
 
+    // p1의 값을 원하는 계산으로 구하기
+    // 둘다 최대가 100이라고 치면
+    // p1의 위치는 x의 40부분, y의 90부분
+    float p1_x = (roi_x / 10);
+    float p1_y = (roi_y / 10);
 
-    // 차선 침범한게 확실하면 result를 1으로 한다.
-    // 어차피 while문 안에서 반복되기 때문에 초기화 걱정 안해도됨
-    int result = 0;
+    // 원하는 ROI부분 설정하기
+    Rect rect(p1_x * 4, p1_y * 9, p1_x * 2, p1_y);
 
-    // line을 detection한다.
+    // roi부분을 frame에 넣기
+    Mat roi = frame(rect);
 
-    // line departure 텍스트 출력한다.    
-    if(result == 1){
-        // 프레임 화면에 표시하기
-        // 매 프레임마다 출력이 될 것이고, 차선을 벗어나면 없어질 것이다. 
-        putText(frame, "Line departure!",Point(150, 100), FONT_HERSHEY_COMPLEX, 1.5, Scalar(0, 0, 255), 2, LINE_AA, false);
+    // 해당 부분을 가우시안해서 명확하게 바꾸기 그 다음에 blur에 저장
+    GaussianBlur(roi, blur, Size(5, 5), 5, 5, BORDER_DEFAULT);
+    
+    // 블러한 이미지를 케니엣지를 통 하여서 선을 구한다. 
+    // blur - > line
+    Canny(blur, line, 60, 180, 3);
+
+    // 캐니로 구한 선을 허프 변환 
+    HoughLinesP(line, lines, 1, CV_PI / 180, 50, 10, 300);
+
+    // 허프변환이 리턴한 lines 백터의 사이즈가 1이상 0일때가 있을 것이다. 
+    // 차선을 감지한다면 line는 1이상일 것이고 차선을 감지하지 못한다면 lines의 값은 0일것이다.
+    // 정해진 ROI에서 차선을 감지했으니 Line daparture 출력한다.
+    if(lines.size() >= 1){
+        putText(frame, "Line departure!", Point(150, 100), FONT_HERSHEY_COMPLEX, 1.5, Scalar(0, 0, 255), 2, LINE_AA, false);
     }
 }
 
 
-// start moving
-// background subtraction을 사용하면 된다. 
-int start_moving(Mat frame){    
-    int result;
+// // start moving
+// // background subtraction을 사용하면 된다. 
+// int start_moving(Mat frame){    
+//     int result;
 
-    if(result == 1){
-        // 프레임 화면에 표시하기
-        putText(frame, "Start Moving!",Point(150, 100), FONT_HERSHEY_COMPLEX, 1.5, Scalar(0, 0, 255), 2, LINE_AA, false);
-    }
-   
-}
+//     if(result == 1){
+//         // 프레임 화면에 표시하기
+//         putText(frame, "Start Moving!",Point(150, 100), FONT_HERSHEY_COMPLEX, 1.5, Scalar(0, 0, 255), 2, LINE_AA, false);
+//     }  
+// }
 
 int main(){
 
@@ -78,7 +95,7 @@ int main(){
 
         // 다음 프레임 읽기
         cap >> frame;
-        
+
         // 프레임 끝나면 끄기
         if(frame.empty()){
             break;
@@ -87,47 +104,12 @@ int main(){
         // linedeparture 감지함수
         line_departure(frame);
         
-        // Start Moving 감지함수
-        start_moving(frame);
+        // // Start Moving 감지함수
+        // start_moving(frame);
 
         imshow("Project2", frame);
         waitKey(delay);
     }
-
-    return 0;
-}
-
-
-
-
-int main() {
-
-    Mat image, edge, result;
-    vector<Vec4i> lines;
-    image = imread("chess_pattern.png");
-    result = image.clone();
-    cvtColor(image, image, CV_BGR2GRAY);
-    Canny(image, edge, 50, 200, 3);
-
-
-    //edge: input Mat, lines: output vector of lines
-    //1: (rho) distance resolution of the accumulator in pixels
-    //CV_PI/180: (theta) angle resolution of the accumulator in radians
-    //50: (threshold) accumulator threshold parameter
-    //10: (minLineLength) minimum line length.
-    //300: (maxLineGap) Maximum allowed gap between points on the sa
-
-    HoughLinesP(edge, lines, 1, CV_PI / 180, 50, 10, 300);
-
-    for (int i = 0; i < lines.size(); i++) {
-        Vec4i l = lines[i];
-        line(result, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, 8);
-    }
-
-    imshow("Input image", image);
-    imshow("edge", edge);
-    imshow("Hough Transform", result);
-    waitKey(0);
 
     return 0;
 }
